986276e8120dfdd90078e1bf813b4474bb66a5f6
uri zafrir <urizaf@gmail.com>
2025-08-11 14:08:27 +0300

    Add flake.nix for NixOS development environment

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "ARandR development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+      packages = with nixpkgs.legacyPackages.x86_64-linux; [
+        python3
+        python3Packages.pygobject3
+        gtk3
+      ];
+    };
+  };
+}
